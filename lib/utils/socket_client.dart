137c71b2c9084de82f8671a8a13f3b8a4f2b365c
Vansh-Bh <151959798+Vansh-Bh@users.noreply.github.com>
2024-06-13 00:31:46 +0530

    Update socket_client.dart

--- lib/utils/socket_client.dart
+++ lib/utils/socket_client.dart
@@ -5,7 +5,7 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = IO.io('http://0.0.0.0:3000', <String, dynamic>{
+    socket = IO.io('http://localhost:3000', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
